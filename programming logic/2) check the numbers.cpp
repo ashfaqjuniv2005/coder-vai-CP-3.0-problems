302e77d6550cd10f4ff95f3981a83744c8a7d5e1
S. M. ASHFAQ HOSSAIN <ashfaqhossain830@gmail.com>
2025-06-16 18:05:13 +0600

    Create 2) check the numbers.cpp

---
+++ programming logic/2) check the numbers.cpp
@@ -0,0 +1,22 @@
+/*
+
+You will be given 2 numbers. Compare them and check who is greater or if they are equal.
+
+Input
+
+Given two integer numbers a, b.
+
+a, b <= 104
+
+Output
+
+If a is greater then print "First", if b is greater print "Second", else print "Equal".
+
+Example
+
+Input
+9 2
+output 
+first
+
+*/
