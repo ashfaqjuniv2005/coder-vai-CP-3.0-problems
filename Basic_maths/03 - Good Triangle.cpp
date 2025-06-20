e5075aad3ae829f231ee4eaafb9860d562ba6973
S. M. ASHFAQ HOSSAIN <ashfaqhossain830@gmail.com>
2025-06-20 18:14:24 +0600

    Update 03 - Good Triangle.cpp

--- Basic_maths/03 - Good Triangle.cpp
+++ Basic_maths/03 - Good Triangle.cpp
@@ -1,3 +1,45 @@
+/*
+You are given the three co-ordinates of a triangle. You need to find the type of the triangle from the co-ordinates. 
+
+Equilateral Triangle: All sides are equal in length.
+
+Isosceles Triangle: Two sides are equal in length, and the third side is different.
+
+Scalene Triangle: All sides are different in length.
+
+Finally print the area and perimeter of the triangle rounded to 4 decimal places.
+
+Input
+
+Co-ordinate of first point x1, y1
+
+Co-ordinate of second point x2, y2
+
+Co-ordinate of third point x3, y3 
+
+All values have absolute value less than 106.
+
+Output
+
+Output the type of the triangle in the first line.
+
+In the second line output the area and perimeter (rounded to 4 decimal places).
+
+Example
+
+Input
+0 0
+
+0 3
+
+4 0
+
+Output
+Scalene Triangle
+
+6.0000 12.0000
+    */
+
 #include<bits/stdc++.h>
 using namespace std; 
     
